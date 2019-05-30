5e210e0134176fd0c78e57df62226cf88ed66bb3
fabricio.sobral <fabricio.sobral@fireking.com>
2019-05-30 17:06:31 -0300

    call days into calendar

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -2,79 +2,75 @@ import React from 'react'
 import './calendar.css'
 import { Header, Segment, Table } from 'semantic-ui-react'
 
+let today = new Date()
+let currentMonth = today.getMonth()
+let currentYear = today.getFullYear()
+let monthsOfYear = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+let monthWithYear = monthsOfYear[currentMonth]
+
+function daysInMonth(iMonth, iYear) {
+    return 32 - new Date(iYear, iMonth, 32).getDate();
+}
+
 class CalendarScheduler extends React.Component {
+   
     createTable = () => {
-       // let firstDayOfMonth = (new Date(currentYear, currentMonth)).getDay();
-        // console.log(firstDayOfMonth);
+        let firstDayOfMonth = (new Date(currentYear, currentMonth)).getDay();
+        let ld = (new Date(today.getFullYear(), today.getMonth() + 1, 0));
+        let lastDayOfMonth = ld.getDate();
         let dateDay = 1;
-        // let dayOfTheMonth = []
-        // let weekOfTheMonth = []
         let table = []
         for (let i = 0; i < 6; i++) {
             let children = []
             for (let j = 0; j < 7; j++) {
-                children.push(<Table.Cell>{`Column ${j + 1}`}</Table.Cell>)
-                // if (i === 0 && j < firstDayOfMonth) {
-                //     //console.log("j: "+ j +" ---- i: "+i)
-                // }
-                // else if (dateDay > daysInMonth(currentMonth, currentYear)) {
-                //     break;
-                // }
-                // else {
-                    // console.log(dateDay)
-                    // dayOfTheMonth += <div>{dateDay}</div>
-                    // let myJSON = JSON.stringify(dateDay);
-                    // console.log("json: "+myJSON)
-                    
-                    // if (dateDay === today.getDate() && currentYear === today.getFullYear() && currentMonth === today.getMonth()) {
+                if (i === 0 && j < firstDayOfMonth) {
+                     children.push(<Table.Cell key={j} className="dayOff"></Table.Cell>)
+                }
+                else if (dateDay > daysInMonth(currentMonth, currentYear)) {
+                    break;
+                }
+                else {
+                    if (dateDay === today.getDate() && currentYear === today.getFullYear() && currentMonth === today.getMonth()) {
+                        //here the day of today, need class
+                        console.log(dateDay); //get day today
                         
-                    // }
+                    }
+                    children.push(<Table.Cell key={j} id={dateDay}>{dateDay}</Table.Cell>)
                     dateDay++;
-                    // console.log("++ " + dateDay++)
-                // }
-                table.push(<Table.Row>{children}</Table.Row>)
+
+                    if(dateDay > lastDayOfMonth){
+                        children.push(<Table.Cell key={dateDay} className="dayOff" ></Table.Cell>)
+                    }
+                }
+
             }
+            table.push(<Table.Row key={i} children={children} />)
         }
         return table
     }
-    
-    // function daysInMonth(iMonth, iYear) {
-    //     return 32 - new Date(iYear, iMonth, 32).getDate();
-    // }
+
     render() {
-        let today = new Date()
-        // let currentDay = today.getDate()
-        let currentMonth = today.getMonth()
-        let currentYear = today.getFullYear()
-        let monthsOfYear = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-        let monthWithYear = monthsOfYear[currentMonth] 
-        console.log(today)
         
         const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         const listDays = daysOfWeek.map((dayName) =>
-            <Table.HeaderCell key={dayName.toString()}>{dayName}</Table.HeaderCell> 
+            <Table.HeaderCell key={dayName.toString()}>{dayName}</Table.HeaderCell>
         )
         return (
             <div className="masterContent">
-                {/* <div>{currentDay}</div>
-                <div>{currentMonth}</div> */}
                 <Header as='h2' attached='top' className="titleCaledar">
-                {monthWithYear} {currentYear}
+                    {monthWithYear} {currentYear}
                 </Header>
                 <Segment attached>
                     <Table celled fixed singleLine>
                         <Table.Header>
-                        <Table.Row>
-                            {listDays}
-                        </Table.Row>
+                            <Table.Row>
+                                {listDays}
+                            </Table.Row>
                         </Table.Header>
                         <Table.Body>
-                        {this.createTable()}
-                        {/* <Table.Row>
-                            <Table.Cell>{dayOfTheMonth}</Table.Cell>
-                        </Table.Row> */}
-                    </Table.Body>
-                </Table>
+                            {this.createTable()}
+                        </Table.Body>
+                    </Table>
                 </Segment>
             </div>
         )
